8b4484ff88c648d7c7e8fae8d515f4acccae7f8a
Ngoc Tran <ngoctnq@me.com>
2017-03-30 18:11:54 -0400

    placeholder pys

---
+++ init.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python2
+'''
+Initialize data, either bridging from Wabash's database or input file.
+'''
+
+def import_data():
+   pass
---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python2
+'''
+Oral Comprehensive Assigning Tool
+MAT-226 Final Project
+'''
+# imports
+import init
+
+# initializations
+init.import_data()
